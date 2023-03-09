16e931b7421321eec57ccfd22d6418571966b0db
Santiago Santos <seba00001s@gmail.com>
2023-03-09 13:09:28 -0300

    Decodificacion terminada, faltan algunos detalles

--- Ejercicio3/src/Main.java
+++ Ejercicio3/src/Main.java
@@ -16,8 +16,6 @@ public class Main {
         String codiODeco = args[2];
         int desplazamiento = Integer.parseInt(args[3]);
 
-        System.out.println(rutaEntrada + "\n" +rutaSalida + " \n" + args[2] + "\n" + args[3]);
-
         try {
             for (String line: Files.readAllLines(Paths.get(rutaEntrada))) {
                 codificarODecodificar(line, desplazamiento, rutaSalida, codiODeco);
@@ -39,9 +37,11 @@ public class Main {
 
         if (cd.equalsIgnoreCase("c")){
             palabraCompleta = codificador(aux, n);
+            System.out.println("El archivo se codifico correctamente.");
         } else{
             n = n*-1;
             palabraCompleta = decodificador(aux, n);
+            System.out.println("El archivo se decodifico correctamente.");
         }
 
         Files.writeString(Paths.get(rutaSal), palabraCompleta);
@@ -96,13 +96,15 @@ public class Main {
 
         for(int i = 0; i < aux.length; i++){
 
-            if(aux[i] != ' ' && aux[i] + n <= 122){
+
+            if(aux[i] != ' ' && aux[i] + n >= 97){
                 aux[i] += n;
                 flag = true;
             }
 
-            if(aux[i] + n > 122 && flag == false){
-                resto = 96 + n - (122 - aux[i]);
+            if(aux[i] + n < 97 && flag == false && aux[i] != ' '){
+
+                resto = 123 + n + (aux[i] - 97);
                 aux[i] = (char) resto;
             }
             flag = false;
